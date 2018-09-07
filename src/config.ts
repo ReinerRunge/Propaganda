ee53bec95fdb4e607581f9272b744bdb67469bf8
Reiner Runge <reiner@86degrees.com>
2018-09-07 13:41:29 +0200

    Add firebaseConfig

---
+++ src/config.ts
@@ -0,0 +1,11 @@
+export const  firebaseConfig = {
+    fire:
+        {
+            apiKey: 'AIzaSyDn_QoRM12SO1F0y8Zyqyztlz6AlENNIsM',
+            authDomain: 'auth-52fe6.firebaseapp.com',
+            databaseURL: 'https://auth-52fe6.firebaseio.com',
+            projectId: 'auth-52fe6',
+            storageBucket: 'auth-52fe6.appspot.com',
+            messagingSenderId: '701067875837'
+        }
+};
